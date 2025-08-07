2f2ae42134e37acc485238a10221a7aad081145d
AndreyGalchevski <andreygalchevski@gmail.com>
2025-08-07 15:00:52 +0300

    Add arabic trans

--- lib/i18n/index.ts
+++ lib/i18n/index.ts
@@ -1,11 +1,12 @@
 import * as Localization from 'expo-localization';
 import { I18n } from 'i18n-js';
 
+import ar from './locales/ar.json';
 import en from './locales/en.json';
 import he from './locales/he.json';
 import ru from './locales/ru.json';
 
-const i18n = new I18n({ en, he, ru });
+const i18n = new I18n({ en, he, ru, ar });
 
 i18n.locale = Localization.getLocales()[0]?.languageCode ?? 'en';
 
